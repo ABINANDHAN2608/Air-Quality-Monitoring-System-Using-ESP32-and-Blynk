7f1ee1015eef29c55e8d429b9ca3a5fa0fd96556
ABINANDHAN2608 <145591747+ABINANDHAN2608@users.noreply.github.com>
2026-08-02 23:45:04 +0530

    Add initial project structure for air quality monitoring

---
+++ airqualitymonitoring.ino
@@ -0,0 +1,14 @@
+Air-Quality-Monitoring-System-Using-ESP32-and-Blynk
+│
+├── README.md
+├── LICENSE
+├── images
+│   ├── circuit.png
+│   ├── esp32.jpg
+│   └── output.jpg
+│
+├── code
+│   └── AirQualityMonitoring.ino
+│
+└── docs
+    └── Project_Report.pdf
